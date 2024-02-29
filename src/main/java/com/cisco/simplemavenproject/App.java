13a33cacc2546cca0f138d006b6365f927f3fd4b
pallavika11 <gampalapallavika@gmail.com>
2024-02-29 15:10:06 +0530

    changed

--- src/main/java/com/cisco/simplemavenproject/App.java
+++ src/main/java/com/cisco/simplemavenproject/App.java
@@ -4,7 +4,9 @@ public class App
 {
     public static void main( String[] args )
     {
+    	Hello.print();
         System.out.println( "Welcome to the world of the jenkins !" );
         System.out.println("Simple project for jenkins");
+        System.out.println("Jenkis file is created");
     }
 }
---
+++ src/main/java/com/cisco/simplemavenproject/Hello.java
@@ -0,0 +1,10 @@
+package com.cisco.simplemavenproject;
+
+public class Hello {
+
+	public static void print() {
+		System.out.println("Hello from CI");
+
+	}
+
+}
