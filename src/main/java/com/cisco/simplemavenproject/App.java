6b523146675380960b8b44ac962f748426f8d90b
pallavika11 <gampalapallavika@gmail.com>
2024-02-29 11:45:38 +0530

    first commit for jenkins

---
+++ src/main/java/com/cisco/simplemavenproject/App.java
@@ -0,0 +1,10 @@
+package com.cisco.simplemavenproject;
+
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Welcome to the world of the jenkins !" );
+        System.out.println("Simple project for jenkins");
+    }
+}
